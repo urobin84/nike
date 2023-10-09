d49cc0131d6aca06057f0b6329de71b1fe4bd392
Muhammad  Muqorrobin <muhammad.muqorrobin@mitracomm.com>
2023-10-09 16:52:00 +0700

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+PopularProducts
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReview from "./CustomerReview";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReview,
+  Subscribe,
+  Footer
+}
